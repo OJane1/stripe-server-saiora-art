b12ef907cfdf1e3e2004bc326789d8507e6a3321
OJane1 <60651332+OJane1@users.noreply.github.com>
2025-01-10 11:45:52 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/stripe/charge", cors(), async (req, res) => {
+  console.log("stripe-routes.js 9 | route reached", req.body);
+  let { amount, email, id } = req.body;
+  console.log("stripe-routes.js 10 | amount and id", amount, id);
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: "EUR",
+      description: "Saiora Art",
+      payment_method: id,
+      receipt_email: email,
+      confirm: true,
+      automatic_payment_methods: {
+        enabled: true,
+      },
+      return_url: "http://localhost:3000/payment-success",
+    });
+    console.log("stripe-routes.js 19 | payment", payment);
+    res.json({
+      message: "Paiement effectué avec succès",
+      success: true,
+    });
+  } catch (error) {
+    console.log("stripe-routes.js 17 | error", error);
+    res.json({
+      message: "Payment Failed",
+      success: false,
+    });
+  }
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log("Server started...");
+});
